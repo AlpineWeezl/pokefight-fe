2b166c07e14d6bbd6dbe892e6643888ed9fa4860
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-30 17:00:14 +0200

    add icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGithub,faLinkedin, faPinterest, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
+
 import React from 'react'
 
 const Footer = () => {
@@ -7,9 +10,9 @@ const Footer = () => {
                     This website was created with React and TailwindCSS.
             </div>
             <div className='text-center flex'>
-                <a href='#' className='w-10 h-10 rounded-full bg-lightyellow mx-1 inline-block pt-1'>h</a>
-                <a href='#' className='w-10 h-10 rounded-full bg-lightyellow mx-1 inline-block pt-1'>h</a>
-                <a href='#' className='w-10 h-10 rounded-full bg-lightyellow mx-1 inline-block pt-1'>h</a>
+                <a href='#' className='w-10 h-10 rounded-full bg-lightyellow text-mint mx-1 inline-block pt-2'><FontAwesomeIcon icon={faGithub}/></a>
+                <a href='#' className='w-10 h-10 rounded-full bg-lightyellow text-mint mx-1 inline-block pt-2'><FontAwesomeIcon icon={faLinkedin}/></a>
+                <a href='#' className='w-10 h-10 rounded-full bg-lightyellow text-mint mx-1 inline-block pt-2'><FontAwesomeIcon icon={faYoutube}/></a>
             </div>
         </div>
         
