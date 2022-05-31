31948af13a13fe82d87e5f45ac6717c55d369e0f
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-31 18:05:53 +0200

    add show more functionality

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -4,7 +4,7 @@ import PokeDetails from './PokeDetails';
 import PokePicture from './PokePicture';
 
 const Pokedex = () => {
-    const apiBackendPath = `${process.env.REACT_APP_POKEFIGHT_API}/api/pokemon`;
+    const apiBackendPath = `${process.env.REACT_APP_POKEAPI}?limit=100000&offset=0`;
     //const pokemonPerPage = 18;
     const offset = 0;
     const [loading, setLoading] = useState(true);
@@ -18,24 +18,23 @@ const Pokedex = () => {
     const [searchString, setSearchString] = useState('');
 
     useEffect(() => {
-        if (searchString.length < 3) {
+        if (searchString.length < 3 && !filteredPokemon) {
             axios
                 .get(apiBackendPath)
                 .then(res => {
-                    setPokemon(res.data);
-                    setFilteredPokemon(res.data);
-                    console.log(res.data);
+                    setPokemon(res.data.results);
+                    setFilteredPokemon(res.data.results);
                     setLoading(false)
                 })
                 .catch(err => console.log(err));
         } else {
             setFilteredPokemon(pokemon.filter(
                 
-                filterPoke => filterPoke.name.english.toLowerCase().includes(searchString.toLowerCase())
+                filterPoke => filterPoke.name.toLowerCase().includes(searchString.toLowerCase())
             ));
             setLoading(false);
         }
-    }, [apiBackendPath, offset, pokemon, searchString]);
+    }, [offset, filteredPokemon, pokemon, searchString, apiBackendPath]);
 
 
     const showDetailHandler = (e) => {
@@ -72,10 +71,10 @@ const Pokedex = () => {
                             filteredPokemon.map((snglPokemon, index) => {
                                 return (
                                     (index >= offset && index < offset + pokemonPerPage) && (
-                                        <div key={`Pokemon_${snglPokemon.id}`}>
-                                            <button name={`${snglPokemon.name.english.toLowerCase()}`} onClick={showDetailHandler} className='my-3'>
-                                                <PokePicture id={snglPokemon.id} name={`${snglPokemon.name.english}`} />
-                                                <p>{snglPokemon.name.english}</p>
+                                        <div key={`Pokemon_${snglPokemon.name}`}>
+                                            <button name={`${snglPokemon.name.toLowerCase()}`} onClick={showDetailHandler} className='my-3'>
+                                                <PokePicture id={snglPokemon.name} name={`${snglPokemon.name}`} />
+                                                <p>{snglPokemon.name}</p>
                                             </button>
                                         </div>
                                     )
