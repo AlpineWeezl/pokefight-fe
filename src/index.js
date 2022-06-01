724c0283dc2e7a668d9644fcea405829f7669787
Matthias Wieser <matthias.wieser.83@live.de>
2022-06-01 09:24:23 +0200

    wrap context around app in index.js

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import PlayerState from './context/PlayerName';
+import PokedexState from './context/Pokedex';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <PlayerState>
-                <App />
+                <PokedexState>
+                    <App />
+                </PokedexState>
             </PlayerState>
         </BrowserRouter>
     </React.StrictMode>
