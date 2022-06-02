6f42d85a33d79b1c43eef8fb1d81a41a845c7f88
Matthias Wieser <matthias.wieser.83@live.de>
2022-06-02 21:37:03 +0200

    add details container to arena details

--- src/components/ArenaDetails.js
+++ src/components/ArenaDetails.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { gameContext } from '../context/GameContext'
 
-const ArenaDetails = ({player}) => {
-    
-    
+const ArenaDetails = ({ isPlayer }) => {
+    const { playerPokemon, enemyPokemon } = useContext(gameContext);
+    const [loading, setLoading] = useState(true);
+    const [pokemon, setPokemon] = useState(null);
+
+
+    useEffect(() => {
+        isPlayer ? setPokemon(playerPokemon) : setPokemon(enemyPokemon);
+        pokemon && setLoading(false);
+    }, [isPlayer, playerPokemon, enemyPokemon, pokemon])
 
     return (
-        <div className='border rounded bg-lightblue '>
+        <>
+            {
+                (!loading) && (
 
-        </div>
+                    <div className='border rounded bg-lightblue p-2'>
+                        <table>
+                            <tbody>
+                                {
+                                    pokemon.extended.stats.map(stat => {
+                                        return (
+                                            <tr key={`${stat}`}>
+                                                <td className='text-right py-1 px-4'>{stat.stat.name}</td>
+                                                <td className='pr-4'>{stat.base_stat}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                )
+            }
+        </>
     )
 }
 
