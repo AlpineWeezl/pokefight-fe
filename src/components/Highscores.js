632e51ef4c2126bca9cc524fd58419639b4da8f9
Matthias Wieser <matthias.wieser.83@live.de>
2022-06-03 16:26:48 +0200

    add highscores structure

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const Highscores = () => {
-  return (
-    <div>Highscores</div>
-  )
+    const [players, setPlayers] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const apiString = `${process.env.REACT_APP_POKEFIGHT_API}/highscores`; // <-- nachschauen, welcher Endpunkt
+        axios
+            .get(apiString)
+            .then(res => {
+                setPlayers(res.data.sort((a, b) => a.points - b.point));
+                setLoading(false);
+            })
+            .catch(err => setError(err));
+    }, []);
+
+    return (
+        <>
+            {(error) && <h2>Oh no, an error occured!</h2>}
+            {(!error && loading) && <h2>Loading ...</h2>}
+            {(!error && !loading) && (
+                <>
+                    <h1>Highscores</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Rank</th>
+                                <th>User</th>
+                                <th>Points</th>
+                                <th>Played</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                players.map((player, index) => {
+                                    return (
+                                        <tr key={player._id}>
+                                            <td>{index + 1}</td>
+                                            <td>{player.name}</td>
+                                            <td>{player.points}</td>
+                                            <td>{player.played}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </>
+            )
+            }
+        </>
+    )
 }
 
 export default Highscores
