7d83f4220ae29d3be2bdaaa6fce513fbe196f433
Matthias Wieser <matthias.wieser.83@live.de>
2022-06-03 17:34:39 +0200

    added function and JSX to highscores

--- src/components/Highscores.js
+++ src/components/Highscores.js
@@ -7,11 +7,12 @@ const Highscores = () => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const apiString = `${process.env.REACT_APP_POKEFIGHT_API}/highscores`; // <-- nachschauen, welcher Endpunkt
+        const apiString = `${process.env.REACT_APP_POKEFIGHT_API}/api/highscores`; // <-- nachschauen, welcher Endpunkt
         axios
             .get(apiString)
             .then(res => {
-                setPlayers(res.data.sort((a, b) => a.points - b.point));
+                console.log(res.data.scores);
+                setPlayers(res.data.scores.sort((a, b) => a.points - b.point));
                 setLoading(false);
             })
             .catch(err => setError(err));
@@ -23,31 +24,33 @@ const Highscores = () => {
             {(!error && loading) && <h2>Loading ...</h2>}
             {(!error && !loading) && (
                 <>
-                    <h1>Highscores</h1>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Rank</th>
-                                <th>User</th>
-                                <th>Points</th>
-                                <th>Played</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                players.map((player, index) => {
-                                    return (
-                                        <tr key={player._id}>
-                                            <td>{index + 1}</td>
-                                            <td>{player.name}</td>
-                                            <td>{player.points}</td>
-                                            <td>{player.played}</td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
+                    <h1 className='flex justify-center text-2xl text-center py-5'>Highscores</h1>
+                    <div className='flex justify-center'>
+                        <table className='justify-center divide-y w-full'>
+                            <thead>
+                                <tr className=''>
+                                    <th className='text-right py-2 pr-4 text-lg'>Rank</th>
+                                    <th className='text-left py-2 pr-2 text-lg'>Player</th>
+                                    <th className='text-right py-2 pr-2 text-lg'>Score</th>
+                                    <th className='text-right py-2 pr-2 text-lg'>Rounds</th>
+                                </tr>
+                            </thead>
+                            <tbody className='divide-y'>
+                                {
+                                    players.map((player, index) => {
+                                        return (
+                                            <tr key={player._id} className={`${(index % 2 === 0) && ('bg-skyblue')}`}>
+                                                <td className='text-right py-2 pr-4 text-lg'>{index + 1}</td>
+                                                <td className='text-left py-2 pr-2 text-lg'>{player.player}</td>
+                                                <td className='text-right py-2 pr-2 text-lg'>{player.score}</td>
+                                                <td className='text-right py-2 pr-2 text-lg'>{player.rounds}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </>
             )
             }
